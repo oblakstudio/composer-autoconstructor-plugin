459564a9902b27f1d217cb55d937ece7fa2f3a2c
Sibin Grasic <sibin.grasic@oblak.studio>
2023-07-27 15:20:42 +0200

    feat: Init Commit

--- src/AutoConstructorPlugin.php
+++ src/AutoConstructorPlugin.php
@@ -7,7 +7,7 @@
  * @subpackage Composer
  */
 
-namespace Oblak\Extremis\Composer\Plugin;
+namespace Oblak\Composer\Plugin;
 
 use Composer\Composer;
 use Composer\EventDispatcher\EventSubscriberInterface;
@@ -17,10 +17,21 @@ use Composer\Script\Event;
 use Composer\Script\ScriptEvents;
 use Composer\Util\Filesystem;
 use Composer\Util\ProcessExecutor;
+use ReflectionClass;
 
 class AutoConstructorPlugin implements PluginInterface, EventSubscriberInterface
 {
-    private const MESSAGE_RUNNING_AUTOCONSTRUCTOR = 'Running autoconstructor...';
+    private const MESSAGE_RUNNING_PLUGIN   = 'Running autoconstructor...';
+    private const MESSAGE_NO_CLASSES_FOUND = 'No classes found to add to autoconstructor...';
+
+    /**
+     * Extra keys for the composer.json file.
+     */
+    private const KEY_EXCLUDE_FILE    = 'autoconstructor-exclude-file';
+    private const KEY_EXCLUDE_CLASSES = 'autoconstructor-exclude-classes';
+    private const KEY_CLASS_PATH      = 'autoconstructor-class-path';
+    private const KEY_MODULE_FILE     = 'autoconstructor-module-file';
+    private const KEY_BASE_NAMESPACE  = 'autoconstructor-base-namespace';
 
     /**
      * @var Composer
@@ -36,6 +47,11 @@ class AutoConstructorPlugin implements PluginInterface, EventSubscriberInterface
      * @var Filesystem
      */
     private $filesystem;
+
+    /**
+     * @var string[]
+     */
+    private $excludes;
 
     /**
      * @var array
@@ -76,7 +92,7 @@ class AutoConstructorPlugin implements PluginInterface, EventSubscriberInterface
         $instance->composer = $composer;
 
         $instance->init();
-        $instance->findClasses();
+        $instance->generateFile();
     }
 
     /**
@@ -119,11 +135,37 @@ class AutoConstructorPlugin implements PluginInterface, EventSubscriberInterface
      */
     private function init()
     {
-        $this->cwd     = getcwd();
-        $this->classes = [];
+        $this->cwd      = getcwd();
+        $this->classes  = [];
+        $this->excludes = $this->getExcludes();
 
         $this->processExecutor = new ProcessExecutor($this->io);
         $this->filesystem      = new Filesystem($this->processExecutor);
+    }
+
+    /**
+     * Returns the exclude file path.
+     *
+     * @return string
+     */
+    private function getExcludes(): array
+    {
+        $excludes_file = $this->cwd . '/config/excludes.php';
+        $extra         = $this->composer->getPackage()->getExtra();
+
+        if (isset($extra[self::KEY_EXCLUDE_CLASSES]) && is_array($extra[self::KEY_EXCLUDE_CLASSES])) {
+            return $extra[self::KEY_EXCLUDE_CLASSES];
+        }
+
+        if (isset($extra[self::KEY_EXCLUDE_FILE])) {
+            $excludes_file = $this->cwd . '/config/excludes.php';
+        }
+
+        if (file_exists($excludes_file)) {
+            return include $excludes_file;
+        }
+
+        throw new \InvalidArgumentException('No exclude file or exclude classes found');
     }
 
     /**
@@ -131,12 +173,12 @@ class AutoConstructorPlugin implements PluginInterface, EventSubscriberInterface
      */
     public static function getSubscribedEvents()
     {
-        return array(
-            ScriptEvents::POST_AUTOLOAD_DUMP => array(
-                array('findClasses', 0),
-            ),
+        return [
+            ScriptEvents::POST_AUTOLOAD_DUMP => [
+                ['generateFile', 0],
+            ],
 
-        );
+        ];
     }
 
     /**
@@ -147,14 +189,140 @@ class AutoConstructorPlugin implements PluginInterface, EventSubscriberInterface
      * @throws ProcessFailedException
      * @throws RuntimeException
      */
-    public function findClasses()
+    public function generateFile()
     {
         $io        = $this->io;
         $isVerbose = $io->isVerbose();
         $exitCode  = 0;
 
         if ($isVerbose) {
-            $io->write(sprintf('<info>%s</info>', self::MESSAGE_RUNNING_AUTOCONSTRUCTOR));
+            $io->write(sprintf('<info>%s</info>', self::MESSAGE_RUNNING_PLUGIN));
         }
+
+        $vendorDir = $this->composer->getConfig()->get('vendor-dir');
+
+        $classnames = array_keys(array_filter(
+            (include $vendorDir . '/composer/autoload_classmap.php'),
+            [$this, 'filterClassNames'],
+            ARRAY_FILTER_USE_BOTH,
+        ));
+
+        if (empty($classnames)) {
+            if ($isVerbose) {
+                $io->write(sprintf('<info>%s</info>', self::MESSAGE_NO_CLASSES_FOUND));
+            }
+        }
+
+        $exitCode = $this->createModuleFile($classnames);
+
+        if ($exitCode === 0) {
+            if ($isVerbose) {
+                $io->write('<info>Module file created successfully</info>');
+            }
+            return $exitCode;
+        }
+
+        if ($isVerbose) {
+            $io->write('<error>Module file could not be created</error>');
+        }
+
+        return $exitCode;
+    }
+
+    private function createModuleFile($classnames)
+    {
+        $baseNamespace = $this->getBaseNamespace();
+
+        $output = sprintf(
+            "<?php\n/**\n * %s Modules\n *\n * @package    %s\n * @subpackage Config\n */\n\nreturn array(",
+            $baseNamespace,
+            $baseNamespace,
+        );
+
+        if (empty($classnames)) {
+            $output .= ");\n";
+            return $this->writeFile($output);
+        }
+
+        $max_length = array_reduce($classnames, function ($carry, $classname) {
+            $length = strlen($this->getClassnameKey($classname));
+
+            return ($length > $carry) ? $length : $carry;
+        }, 0);
+
+        foreach ($classnames as $classname) {
+            $key     = $this->getClassnameKey($classname);
+            $output .= sprintf(
+                "\n    '%s'%s => '%s',",
+                $key,
+                str_repeat(' ', $max_length - strlen($key)),
+                str_replace('\\', '\\\\', $classname),
+            );
+        }
+
+        $output .= "\n);\n\n";
+
+        return $this->writeFile($output);
+    }
+
+    private function writeFile(string $output): int
+    {
+        return file_put_contents($this->getModuleFile(), $output) > 0 ? 0 : 1;
+    }
+
+    private function getClassnameKey(string $classname): string
+    {
+        $exploded  = explode('\\', $classname);
+        $class     = strtolower(array_pop($exploded));
+        $namespace = strtolower(array_pop($exploded));
+
+        return "{$namespace}-{$class}";
+    }
+
+    private function getClassPath()
+    {
+        $basePath = $this->cwd . '/framework';
+        $extra    = $this->composer->getPackage()->getExtra();
+
+        if (isset($extra[self::KEY_CLASS_PATH])) {
+            $basePath = $this->cwd . '/' . $extra[self::KEY_CLASS_PATH];
+        }
+
+        return $basePath;
+    }
+
+    private function getBaseNamespace()
+    {
+        $baseNamespace = 'Extremis';
+        $extra         = $this->composer->getPackage()->getExtra();
+
+        if (isset($extra[self::KEY_BASE_NAMESPACE])) {
+            $baseNamespace = $extra[self::KEY_BASE_NAMESPACE];
+        }
+
+        return $baseNamespace;
+    }
+
+    private function getModuleFile()
+    {
+        $modulePath = $this->cwd . '/config/modules.php';
+        $extra      = $this->composer->getPackage()->getExtra();
+
+        if (isset($extra[self::KEY_MODULE_FILE])) {
+            $modulePath = $this->cwd . '/' . $extra[self::KEY_MODULE_FILE];
+        }
+
+        return $modulePath;
+    }
+
+    private function filterClassNames(string $path, string $classname): bool
+    {
+
+        $isAutoconstructable =
+            strpos($classname, $this->getBaseNamespace(), 0) !== false &&
+            strpos($path, $this->getClassPath(), 0) !== false &&
+            (new ReflectionClass($classname))->isInstantiable();
+
+        return $isAutoconstructable && !in_array($classname, $this->excludes);
     }
 }
